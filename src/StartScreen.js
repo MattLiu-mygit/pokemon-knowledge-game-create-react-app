3a17f598d7da52dc1f534efbd7e65e584ef6da23
Matthew Liu <mliu78@jh.edu>
2020-06-19 04:48:10 -0400

    Update StartScreen.js

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -4,7 +4,7 @@ import React from 'react';
 const StartScreen = (props) => {
     return <div>
         <img className = 'pokeball' src = 'https://lh3.googleusercontent.com/proxy/11e9U5lJQtoMP4aZSDCsIgNv8BokC9P0K2a4FL34P_9DBgiZnQndgIZFh1GyrwDGPZZ7Q5ci71SI2EysAHE8M0He70F57OvWiM98FANFeuh9HXEfumjBntd3RvDAkWMhBVlg' />
-        <div className = 'description'>A Pokemon knowledge quiz game made with React.js and the PokeAPI, the Restful Pokemon API, developed by Matthew Liu and (future hopefully) css by Kevin Liu.</div>
+        <div className = 'description'>A Pokemon knowledge quiz game made with React.js and the PokeAPI, the Restful Pokemon API, developed by Matthew Liu.</div>
         <div className = 'instructions'>
             <div>Test your Pokemon knowledge! In 60 seconds, try to match as many Pokemon to the characteristic thrown atcha! If your characteristic is too hard, feel free to reroll a new characteristic to test! Don't worry, you keep your score, even after rerolling! Be aware of the 5 second reroll timer huehuehue!</div>
             <div className = 'finishGameInstruction'>Once you finish your game, have some fun giving the API some attributes to match in the results page!</div>
